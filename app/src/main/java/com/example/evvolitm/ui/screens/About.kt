36ebdb218b0447026d2bafadfe035df7b541c760
Charymyrat Garryyev <kcharymyrat@gmail.com>
2023-12-28 12:32:13 +0500

    About screens map, phone and email intent are done

--- app/src/main/java/com/example/evvolitm/ui/screens/About.kt
+++ app/src/main/java/com/example/evvolitm/ui/screens/About.kt
@@ -1,8 +1,12 @@
 package com.example.evvolitm.ui.screens
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Phone
@@ -12,10 +16,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.evvolitm.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -24,18 +31,13 @@ fun AboutScreen() {
     println(AppCompatDelegate.getApplicationLocales())
     println("AppCompatDelegate.getApplicationLocales()[0]?.language " +
             "= ${AppCompatDelegate.getApplicationLocales()[0]?.language}")
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(title = { Text(stringResource(R.string.about_us)) })
-        }
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .padding(paddingValues)
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
             CompanyLogo()
             Spacer(modifier = Modifier.height(16.dp))
             CompanyDescription()
@@ -65,28 +67,127 @@ fun CompanyDescription() {
 
 @Composable
 fun ContactInformation() {
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
+    Column {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
         ) {
-            ContactDetail(Icons.Default.Place, "123 Tech Street, Innovation City")
-            Spacer(modifier = Modifier.height(8.dp))
-            ContactDetail(Icons.Default.Phone, "+123 456 7890")
-            Spacer(modifier = Modifier.height(8.dp))
-            ContactDetail(Icons.Default.Email, "contact@evvoli.com")
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                val latitude: Double = 37.94038485947134
+                val longitude: Double = 58.42375763825806
+                LocationDetail(
+                    icon = Icons.Default.Place,
+                    detail = "Hoja Ahmet Yasawy St, Ashgabat 744000, Turkmenistan",
+                    latitude = latitude,
+                    longitude = longitude,
+                )
+                PhoneDetail(Icons.Default.Phone, "+993 65 726468")
+                EmailDetail(Icons.Default.Email, "contact@evvoli.com")
+            }
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                val latitude: Double = 37.59980287678001
+                val longitude: Double = 61.87317372474693
+                LocationDetail(
+                    icon = Icons.Default.Place,
+                    detail = "HVXF+V7F, Mary, Turkmenistan",
+                    latitude = latitude,
+                    longitude = longitude,
+                )
+                PhoneDetail(Icons.Default.Phone, "+993 61 004770")
+                EmailDetail(Icons.Default.Email, "contact@evvoli.com")
+            }
+        }
+
+    }
+
+}
+
+
+@Composable
+fun LocationDetail(icon: ImageVector, detail: String, latitude: Double, longitude: Double) {
+    val context = LocalContext.current
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(
+            onClick = {
+                val gmmIntentUri = Uri.parse(
+                    "https://www.google.com/maps/search/?api=1&query=$latitude,$longitude"
+                )
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                context.startActivity(mapIntent)
+            },
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = detail,
+            )
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(detail, style = MaterialTheme.typography.bodyMedium)
     }
 }
 
 @Composable
-fun ContactDetail(icon: ImageVector, detail: String) {
+fun PhoneDetail(icon: ImageVector, detail: String) {
+    val context = LocalContext.current
+
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(icon, contentDescription = null)
+        IconButton(
+            onClick = {
+                val intent = Intent(Intent.ACTION_DIAL).apply {
+                    data = Uri.parse("tel:$detail")
+                }
+                context.startActivity(intent)
+            },
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = detail,
+            )
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(detail, style = MaterialTheme.typography.bodyMedium)
+    }
+}
+
+
+@Composable
+fun EmailDetail(icon: ImageVector, detail: String) {
+    val context = LocalContext.current
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(
+            onClick = {
+                val intent = Intent(Intent.ACTION_SENDTO).apply {
+                    data = Uri.parse("mailto:$detail")
+                }
+                if (intent.resolveActivity(context.packageManager) != null) {
+                    context.startActivity(intent)
+                }
+            },
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = detail,
+            )
+        }
         Spacer(modifier = Modifier.width(8.dp))
         Text(detail, style = MaterialTheme.typography.bodyMedium)
     }
